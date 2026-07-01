350ea16f02c0301c3f68c2e85270d0efef4bb158
Muhammet Alperen Doğan <muhammet.dogan18@ogr.sakarya.edu.tr>
2026-07-01 12:58:53 +0300

    feat(treasury): implement treasury contract with governance-gated withdrawals

---
+++ contracts/treasury/src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, Address, Env, Symbol, token};
+
+#[cfg(test)]
+mod tests;
+
+#[contract]
+pub struct Treasury;
+
+#[contractimpl]
+impl Treasury {
+    pub fn init(env: Env, governance: Address) {
+        env.storage().instance().set(&Symbol::new(&env, "governance"), &governance);
+    }
+
+    pub fn set_governance(env: Env, new_governance: Address) {
+        let current_governance: Address = env.storage().instance().get(&Symbol::new(&env, "governance")).unwrap();
+        current_governance.require_auth();
+        env.storage().instance().set(&Symbol::new(&env, "governance"), &new_governance);
+    }
+
+    pub fn withdraw(env: Env, recipient: Address, amount: i128, token_admin: Address) {
+        let governance: Address = env.storage().instance().get(&Symbol::new(&env, "governance")).unwrap();
+        governance.require_auth();
+        
+        let client = token::Client::new(&env, &token_admin);
+        client.transfer(&env.current_contract_address(), &recipient, &amount);
+    }
+
+    pub fn get_governance(env: Env) -> Address {
+        env.storage().instance().get(&Symbol::new(&env, "governance")).unwrap()
+    }
+}
